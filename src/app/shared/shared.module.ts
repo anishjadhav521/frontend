8280e607dcc62bd2351804be2d9cbe60b0aba9de
anishjadhav521 <anishjadhav5003@gmail.com>
2025-04-03 21:27:11 +0530

    home

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { SharedRoutingModule } from './shared-routing.module';
 import { FollowersComponent } from './components/followers/followers.component';
 import { FollowingComponent } from './components/following/following.component';
+import { CommentsComponent } from './components/comments/comments.component';
 
 
 @NgModule({
   declarations: [
     FollowersComponent,
-    FollowingComponent
+    FollowingComponent,
+    CommentsComponent
   ],
   imports: [
     CommonModule,
